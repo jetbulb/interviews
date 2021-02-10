c036c961ddf0d59f09a554e172969fad75aaaf08
wespe <dobmax90@gmail.com>
2021-02-10 17:28:22 +0100

    develop: add test classes on behalf of mvn layer

---
+++ src/test/java/FirstUnitTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstUnitTest {
+    @Test
+    public void whenThis_thenThat() {
+        Assertions.assertTrue(false);
+    }
+
+    @Test
+    public void whenSomething_thenSomething() {
+        Assertions.assertTrue(false);
+    }
+
+    @Test
+    public void whenSomethingElse_thenSomethingElse() {
+        Assertions.assertTrue(true);
+    }
+}
