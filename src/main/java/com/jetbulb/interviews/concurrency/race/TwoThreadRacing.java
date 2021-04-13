eb66ab5821ced20378b9e7f770ee4a040b0fc22e
wespe <m.dobrynin@raccoonsoft.ru>
2021-04-13 22:36:50 +0200

    feature/concurrency: Counter task

---
+++ src/main/java/com/jetbulb/interviews/concurrency/race/TwoThreadRacing.java
@@ -0,0 +1,22 @@
+package com.jetbulb.interviews.concurrency.race;
+
+public class TwoThreadRacing {
+    public static void main(String[] args) {
+        new Thread(new Counter()).start();
+        new Thread(new Counter()).start();
+
+    }
+
+}
+
+class Counter implements Runnable {
+    public static int c;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            c++;
+        }
+        System.out.printf("%s : %s%n", Thread.currentThread().getName(), c);
+    }
+}
